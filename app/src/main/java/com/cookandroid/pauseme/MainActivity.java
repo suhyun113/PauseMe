1d1996d07c70415f6e0e2d5be04d8fa2bc9bc203
suhyun113 <163711629+suhyun113@users.noreply.github.com>
2025-12-01 19:51:20 +0900

    fix: 로그인 화면 호출 수정

--- app/src/main/java/com/cookandroid/pauseme/MainActivity.java
+++ app/src/main/java/com/cookandroid/pauseme/MainActivity.java
@@ -5,17 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
-import com.cookandroid.pauseme.login.LoginActivity;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
         // 로그인 화면 호출
-        startActivity(new Intent(this, LoginActivity.class));
+        Intent intent = new Intent(this, com.cookandroid.pauseme.LoginActivity.class);
+        startActivity(intent);
         finish();
     }
 }
