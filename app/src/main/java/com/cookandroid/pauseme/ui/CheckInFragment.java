ada0c990a8628384559802b63de12c39959b940c
suhyun113 <163711629+suhyun113@users.noreply.github.com>
2025-12-03 03:11:53 +0900

    feat: 체크인 페이지 UI 구현

--- app/src/main/java/com/cookandroid/pauseme/ui/CheckInFragment.java
+++ app/src/main/java/com/cookandroid/pauseme/ui/CheckInFragment.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CalendarView;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,13 +14,35 @@ import androidx.fragment.app.Fragment;
 
 import com.cookandroid.pauseme.R;
 
+import java.util.Calendar;
+
 public class CheckInFragment extends Fragment {
+
+    private CalendarView calendarView;
+    private TextView txtMonthTitle;
+    private ImageView btnPrevMonth, btnNextMonth;
+
+    private Calendar currentCal;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_checkin, container, false);
+
+        View root = inflater.inflate(R.layout.fragment_checkin, container, false);
+
+        // include 안의 뷰들 연결
+        calendarView   = root.findViewById(R.id.calendar_view);
+
+        return root;
+    }
+
+    private void updateMonthTitle() {
+        int year  = currentCal.get(Calendar.YEAR);
+        int month = currentCal.get(Calendar.MONTH) + 1; // Calendar는 0부터 시작
+        String text = year + "년 " + month + "월";
+        txtMonthTitle.setText(text);
     }
 }
+
